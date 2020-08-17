bcfd6901256db67987a8725aaaa9be4f242db89f
prit <prit.sorathiya@gmail.com>
2020-08-17 22:42:46 +0530

    "done setup alan AI platform"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = '8037c1ae453fa7068039b786259722472e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if (command === 'testCommand') {
+                    alert('This code was executed');
+                }
+            }
+        })
+    }, [])
+
     return (
         <div>
             <h1>Alan AI News Applications</h1>
