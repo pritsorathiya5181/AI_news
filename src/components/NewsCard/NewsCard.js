eeb2bf281c944363a540e48472d795157d298c31
prit <prit.sorathiya@gmail.com>
2020-08-18 12:58:18 +0530

    "implement material Ui/core and /style"

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core';
 
-function NewsCard() {
+import React from 'react';
+
+const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, i }) => {
     return (
-        <div>
-            <h1>News Cards</h1>
-        </div>
+        <Card>
+            <CardActionArea>
+                <CardMedia image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'} />
+                <div>
+                    <Typography variant="body2" color="textSecondary" component="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="h2">{source.name}</Typography>
+                </div>
+                <Typography gutterBottom variant="h5" component="h2">{title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary" >Read More</Button>
+                <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
+            </CardActions>
+        </Card>
     )
 }
 
