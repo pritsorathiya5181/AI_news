29fd0f8c12b9f78eca7f6035d47dc62358a171e9
prit <prit.sorathiya@gmail.com>
2020-08-20 12:12:15 +0530

    update few more features.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import NewsCards from './components/NewsCards/NewsCards';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import useStyles from './styles';
+import wordsToNumbers from 'words-to-numbers';
 
 const alanKey = '8037c1ae453fa7068039b786259722472e956eca572e1d8b807a3e2338fdd0dc/stage';
 
@@ -14,13 +15,25 @@ const App = () => {
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if (command === 'newHeadlines') {
                     console.log(articles);
                     setNewsArticles(articles);
                     setActiveArticle(-1);
                 } else if (command === 'highlight') {
                     setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+                } else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if (parsedNumber > 20) {
+                        alanBtn().playText('Please try that again...');
+                    } else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    } else {
+                        alanBtn().playText('Please try that again...');
+                    }
                 }
             }
         })
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,13 +1,29 @@
 import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core';
+import React, { createRef, useEffect, useState } from 'react';
 
-import React from 'react';
 import classNames from 'classnames'
 import useStyles from './styles';
 
 const NewsCard = ({ article: { description, publishedAt, source, title, url, urlToImage }, activeArticle, i }) => {
     const classes = useStyles();
+    const [elRefs, setElRefs] = useState([]);
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        window.scroll(0, 0);
+
+        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if (i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs]);
+
+
     return (
-        <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
             <CardActionArea href={url} target="_blank">
                 <CardMedia className={classes.media} image={urlToImage || 'https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png'} title={title} />
                 <div className={classes.details}>
